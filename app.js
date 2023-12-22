67ea709c6e8b35bd420d2fa13eb5f4c3f7fc22a9
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2023-12-22 17:55:51 +0100

    “finished_project”

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 require('express-async-errors');
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require('express');
 const app = express();
+
+
 
 // connectDB
 const connectDB = require("./db/connect");
@@ -16,8 +24,16 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set("trust proxy", 1);
+app.use(rateLimiter({
+  windowsMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+}));
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
